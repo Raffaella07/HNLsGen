3442019dd14113faeeb5c300225d9ec875dc670c
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2020-07-06 09:45:27 +0200

    added V06 points

---
+++ slurm/V06_points.py
@@ -0,0 +1,29 @@
+import sys
+sys.path.append('../python/.')
+from common import Point
+
+# List of points to be generated
+#   - N.B.: mass must be a float
+
+points = [
+Point(mass=0.5,ctau=None,vv=1e-04),
+
+Point(mass=1.0,ctau=None,vv=1e-04),
+
+Point(mass=1.5,ctau=None,vv=1e-04),
+
+Point(mass=2.0,ctau=None,vv=1e-04),
+
+Point(mass=2.5,ctau=None,vv=1e-04),
+
+Point(mass=3.0,ctau=None,vv=1e-04),
+
+]
+
+
+#for p in points:
+#  p.stamp()
+
+
+
+
