d850df7fd9f928b6c0ab7b52ff444e8ab2ee8d24
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2020-09-24 19:20:59 +0200

    benchmark points

---
+++ slurm/V10_points.py
@@ -0,0 +1,20 @@
+from python.common  import Point
+
+# List of points to be generated
+#   - N.B.: mass must be a float
+
+points = [
+
+Point(mass=1.0,ctau=None,vv=1e-05),
+Point(mass=2.0,ctau=None,vv=1e-05),
+Point(mass=3.0,ctau=None,vv=5e-05),
+
+]
+
+
+#for p in points:
+#  p.stamp()
+
+
+
+
